6eea13fde23e5e89e349bc74b6253b49ac899bdf
Alvaro Garzón <alvaro.develops@gmail.com>
2022-11-04 23:41:57 +0100

    feat: pagination + navigation complete

--- js/swiperRoomList.js
+++ js/swiperRoomList.js
@@ -5,8 +5,12 @@ const swiper = new Swiper(".swiper-room-list", {
     autoplay: {
         delay: 5000,
     },
+    navigation: {
+        nextEl: '.swiper-button-room-list-next',
+        prevEl: '.swiper-button-room-list-prev',
+    },
     pagination: {
-        el: ".swiper-pagination",
+        el: ".swiper-button-pagination",
         clickable: true,
         renderBullet: function (index, className) {
             return '<span class="' + className + '">' + (index + 1) + "</span>";
