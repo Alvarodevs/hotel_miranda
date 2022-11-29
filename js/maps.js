1fed683e23ad1e5c3ef494f006f447719e0b663a
Alvaro Garzón <alvaro.develops@gmail.com>
2022-11-29 23:27:33 +0100

    feat: styles in map inputs, fixed redirection to html pages

--- js/maps.js
+++ js/maps.js
@@ -1,44 +1,5 @@
-// import { hotels } from '../utils/hotels' assert {type: "json"};
-// import {borders} from '../utils/comunitiesBorders' assert {type: "json"};
-// import { regions } from '../utils/comunitiesBorders' assert {type: "json"};
-// const hotels = [
-// 	{ name: "Hotel Don Paco", lat: 36.71160, lng: -4.42919, ca: "Andalucía" },
-// 	{ name: "Hotel AC Málaga Palacio", lat: 36.71890, lng: -4.41939, ca: "Andalucía" },
-// 	{ name: "Hotel Meliá la Caleta", lat: 36.50820, lng: -6.27982, ca: "Andalucía" },
-// 	{ name: "Hotel Puertatierra", lat: 36.52040, lng: -6.28475, ca: "Andalucía" },
-// 	{ name: "Parador Cádiz", lat: 36.53488711199456, lng: -6.305710743867392, ca: "Andalucía" },
-// 	{ name: "Pestana Plaza Mayor", lat: 40.41520939793109, lng: -3.7069483449347613, ca: "Madrid, Comunidad de" },
-// 	{ name: "Hotel Freedom", lat: 40.412586532955906, lng: -3.6994716832546524, ca: "Madrid, Comunidad de" },
-// 	{ name: "Hotel Francisco I", lat: 40.41798758052421, lng: -3.707637320544922, ca: "Madrid, Comunidad de" },
-// 	{ name: "Hotel Majestic", lat: 41.39391593330566, lng: 2.1639243155412435, ca: "Cataluña / Catalunya" },
-// 	{ name: "ME Barcelona", lat: 41.38937638025521, lng: 2.1699753791207215, ca: "Cataluña / Catalunya" },
-// 	{ name: "Hotel Lasala Plaza", lat: 43.322765911679355, lng: -1.986712217210628, ca: "País Vasco / Euskadi" },
-// 	{ name: "Hotel Villa Favorita", lat: 43.317583017768065, lng: -1.9865405558324714, ca: "País Vasco / Euskadi" },
-// ]
-// const comunidadesAutonomas = [
-// 	'Andalucía',
-// 	'Aragón',
-// 	'Asturias, Principado de',
-// 	'Balears, Illes',
-// 	'Canarias',
-// 	'Cantabria',
-// 	'Castilla y León',
-// 	'Castilla - La Mancha',
-// 	'Cataluña / Catalunya',
-// 	'Comunitat Valenciana',
-// 	'Extremadura',
-// 	'Galicia',
-// 	'Madrid, Comunidad de',
-// 	'Murcia, Región de',
-// 	'Navarra, Comunidad Foral de',
-// 	'País Vasco / Euskadi',
-// 	'Rioja, La',
-// 	'Ceuta',
-// 	'Melilla',
-// ];
-
-
 let map, infoWindow;
+
 function initMap() {
 
 	//Initial position (Main Miranda Hotel, located in Madrid)
@@ -214,7 +175,7 @@ function initMap() {
 	}
 
 	/** FRONTERA COMUNIDADES */
-	const selectComunidades = document.getElementById('comunidades');
+	const selectComunidades = document.getElementById('regions');
 	let regionNum = 0;
 	let regionArea;
 
